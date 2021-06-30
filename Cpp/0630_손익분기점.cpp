9c63efb5249f30a3a5f77e3b2e267dfda4fb2baa
ghd0276 <ghd0276@naver.com>
2021-06-30 19:35:27 +0900

    6/30 손익분기점 완료. 피보나치 수열 도전 중

---
+++ Cpp/0630_손익분기점.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	long A, B, C;
+	long long ans;
+
+	cin >> A >> B >> C;
+
+	if (B >= C)
+	{
+		ans = -1;
+	}
+	else
+	{
+		ans = (A / (C - B)) + 1;
+	}
+
+	cout << ans;
+
+	return 0;
+}
---
+++ Cpp/0630_피보나치수.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int makeFibo(int n);
+
+int main()
+{
+    int n = 3;
+    int fn;
+    int answer;
+
+    fn = makeFibo(5);
+    answer = fn;
+
+    cout << fn << '\n' << answer;
+
+    return 0;
+}
+
+int makeFibo(int n)
+{
+    int fn;
+
+    if (n == 2 || n == 1)
+    {
+        return 1;
+    }
+    else if (n == 0)
+    {
+        return 0;
+    }
+    else if (n > 2)
+    {
+        fn = makeFibo(n - 1) % 1234567 + makeFibo(n - 2) % 1234567;
+        return fn;
+    }
+}
