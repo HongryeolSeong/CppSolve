be47af7bbf4c977860706ada5199a0104c365ce9
HongryeolSeong <76774936+HongryeolSeong@users.noreply.github.com>
2021-07-01 17:10:00 +0900

    Update 0604_내적.cpp

--- Cpp/0604_내적.cpp
+++ Cpp/0604_내적.cpp
@@ -9,7 +9,7 @@ int solution(vector<int> a, vector<int> b) {
 
     for (i = 0; i < a.size(); i++)
     {
-        res += a[i] * b[i];
+        res += a[i] * b[i];         // 벡터를 이용한 내적
     }
 
     return res;
