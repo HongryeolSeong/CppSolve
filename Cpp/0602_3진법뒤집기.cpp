4caf9ca301939add336dc2b95d7a3314d302593d
ghd0276 <ghd0276@naver.com>
2021-07-15 00:08:04 +0900

    7/15 124나라의 숫자 완료

--- Cpp/0602_3진법뒤집기.cpp
+++ Cpp/0602_3진법뒤집기.cpp
@@ -14,14 +14,14 @@ int solution(int n) {
 	while (1)
 	{
 		ary[index] = n % 3;	// ary[0]을 시작으로 3진수 첫 자리부터 대입 시작
-		n /= 3;			// 다음 피제수
+		n /= 3;				// 다음 피제수
 		if (n == 0) break;	// 피제수가 0이면(3진수 변환 완료) 반복문 탈출
-		index++;		// 3진수 변환마다 +1
+		index++;			// 3진수 변환마다 +1
 	}
 
 	// 2. n(3)을 앞 뒤 반전. ary 앞 뒤 반전
-	int temp;				// swap을 위한 임시 변수
-	int j = index;				// ary의 마지막 인덱스
+	int temp;							// swap을 위한 임시 변수
+	int j = index;						// ary의 마지막 인덱스
 	for (i = 0; i <= index / 2; i++)	// 앞뒤 반전을 위해 처음부터 배열의 중간까지 수행
 	{
 		//swap
@@ -35,8 +35,8 @@ int solution(int n) {
 	int three = 1;					// 3진수 변환을 3의 거듭제곱을 담을 변수
 	for (i = 0; i <= index; i++)
 	{
-		res += ary[i] * three;			// 3진수 변환
-		three *= 3;				// 다음 거듭제곱 만들기
+		res += ary[i] * three;		// 3진수 변환
+		three *= 3;					// 다음 거듭제곱 만들기
 	}
 	
 	return res;
